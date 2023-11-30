f0b51de694608e255d7d0cb74e54af146c103427
minhhotboy9x <minhnguyenquocnhat@gmail.com>
2023-11-30 18:05:50 +0700

    change source data and finish movie data

--- crawler.py
+++ crawler.py
@@ -28,10 +28,44 @@ class MovieDataset:
     
     # def get_main_actors(self, id)
 
+class MovieDB:
+    # get each batch data
+    def __init__(self) -> None:
+        self.url = "https://api.themoviedb.org/3"
+        self.headers = {
+            "accept": "application/json",
+            "Authorization": "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI3M2ZkOTg0MmMzMTg2ZDY2OWMxNmEwNmIzYTdjODA2OCIsInN1YiI6IjY1M2IyMzFlMTA5Y2QwMDEyY2ZlMWU2NCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.5nkctErbJj6FNm__3T8rX1iFXSFw5Qasd3JOXqGOKYU"
+        }
+        self.genres = self.get_genres()
+
+    def get_actors(self, page=1):
+        url_actors = self.url + f"/person/popular?page={page}"
+        response = requests.get(url_actors, headers=self.headers)
+        return response.json()['results']
+
+    def get_genres(self):
+        url_genres = self.url + f"/genre/movie/list?language=en"
+        response = requests.get(url_genres, headers=self.headers)
+        return response.json()["genres"]
+
+    def get_movies(self, primary_release_date_gt="1975-04-30", page=1):
+        url_movies = self.url + "/discover/movie?"
+        querystring = {
+            "include_adult": "true",
+            "include_video": "false",
+            "language": "en-US",
+            "page": page,
+            "primary_release_date.gte": primary_release_date_gt,
+            "vote_count.gte": 1000,
+        }
+        response = requests.get(url_movies, headers=self.headers, params=querystring)
+        return response.json()['results']
+
+
 if __name__ == "__main__":
-    movies = MovieDataset()
+    movies = MovieDB()
     mv_json = movies.get_movies()
     for mv in mv_json:
-        print(mv.get('huhu'))
+        # print(mv.get('ratingsSummary'))
         print(json.dumps(mv, indent=2))
         break
--- movie_consumer.py
+++ movie_consumer.py
@@ -1,7 +1,13 @@
 from pyspark.sql import SparkSession
-from pyspark.sql.functions import explode
-from pyspark.sql.functions import split
+from pyspark.sql.types import StructField, StructType, StringType, LongType, \
+                                FloatType, IntegerType, DoubleType, DecimalType, \
+                                ArrayType, BooleanType, DateType
+from pyspark.sql.functions import explode, col, udf
+from pyspark.sql.functions import split, expr
+from crawler import MovieDataset, MovieDB
 import os
+import json
+
 scala_version = '2.12'
 spark_version = '3.5.0'
 # TODO: Ensure match above values match the correct versions
@@ -13,12 +19,64 @@ packages = [
 ]
 # Tạo một phiên Spark
 spark = SparkSession.builder \
-   .master("spark://172.18.32.1:7077") \
-   .appName("movie consumer") \
-   .config("spark.jars.packages", ",".join(packages)) \
+   .master("local") \
+   .appName("Movie Consumer") \
    .getOrCreate()
 
 spark.sparkContext.setLogLevel("ERROR")
-# Địa chỉ của Kafka broker
-# kafka_brokers = "localhost:9092"
-kafka_brokers = "localhost:9095"
+
+@udf(ArrayType(StringType()))
+def map_genre_names(genre_ids):
+    genre_names = [row['name'] for row in df_genre.collect() if row['genre_id'] in genre_ids]
+    return genre_names
+
+# Define the schema
+movie_schema = StructType([
+    StructField("id", StringType(), nullable=False),
+    StructField("adult", BooleanType(), nullable=False),
+    StructField("genre_ids", ArrayType(IntegerType()), nullable=True),
+    StructField("original_language", StringType(), nullable=True),
+    StructField("overview", StringType(), nullable=True),
+    StructField("popularity", DoubleType(), nullable=True),
+    StructField("release_date", StringType(), nullable=True),
+    StructField("title", StringType(), nullable=True),
+    StructField("vote_average", StringType(), nullable=True),
+    StructField("vote_count", IntegerType(), nullable=True),
+])
+
+genre_schema = StructType([
+    StructField("id", IntegerType(), nullable=True),
+    StructField("name", StringType(), nullable=True)
+])
+
+
+# Get movie data
+movies = MovieDB()
+mv_json = movies.get_movies(page=1)
+
+# Create DataFrame
+df_movie = spark.createDataFrame(mv_json, schema=movie_schema)
+
+df_genre = spark.createDataFrame(movies.genres, schema=genre_schema)
+
+df_genre.createOrReplaceTempView("df_genre")
+
+df_movie = df_movie.withColumn("vote_average", col("vote_average").cast("float"))
+
+# Explode the genre_ids array
+df_exploded = df_movie.select(col("id").alias("id_mv"), "genre_ids").withColumn("genre_id", explode("genre_ids"))
+
+# Join with df_genre to get genre_names
+df_result = df_exploded.join(df_genre, df_exploded["genre_id"] == df_genre["id"]) \
+        .withColumnRenamed('genre_id', '_genre_id')  \
+        .groupBy("id_mv").agg({"name": "collect_list"})
+
+# Rename the column
+df_result = df_result.withColumnRenamed("collect_list(name)", "genres")
+
+# Join the result back to the original dataframe
+df_movie = df_movie.join(df_result, df_movie['id'] == df_result['id_mv'] , "left")
+
+df_movie = df_movie.drop("genre_ids").drop("id_mv")
+
+df_movie.show()
