620ca930c1c831c675c946b39b41202a35629afa
minhhotboy9x <minhnguyenquocnhat@gmail.com>
2023-12-18 22:35:44 +0700

    still bug

--- app/actor_consumer.py
+++ app/actor_consumer.py
@@ -13,7 +13,24 @@ MASTER = 'local'
 # MASTER = 'spark://172.28.240.1:7077'
 KAFKA_BROKER1 = os.environ["KAFKA_BROKER1"]
 ACTOR_TOPIC= os.environ["ACTOR_TOPIC"]
+ES_NODES = os.environ['ES_NODES']
+ES_RESOURCE = "actor"
 gender_path = 'genders.json'
+
+#----------------------------------------------
+
+def write_to_elasticsearch(df, epoch_id):
+    df.show()
+    df.write \
+        .format("org.elasticsearch.spark.sql") \
+        .option("es.nodes", ES_NODES) \
+        .option("es.resource", ES_RESOURCE) \
+        .option("es.index.auto.create", "true") \
+        .option("es.nodes.wan.only", "true") \
+        .mode("append") \
+        .save(ES_RESOURCE)
+    
+#----------------------------------------------
 
 with open(gender_path, 'r') as f:
     gender_names = json.loads(f.read())
@@ -24,6 +41,7 @@ packages = [
     f'org.apache.spark:spark-sql-kafka-0-10_{scala_version}:{spark_version}',
     'org.apache.kafka:kafka-clients:3.5.0',
     'org.apache.hadoop:hadoop-client:3.0.0',
+    'org.elasticsearch:elasticsearch-spark-30_2.12:7.17.16'
 ]
 
 spark = SparkSession.builder \
@@ -31,6 +49,7 @@ spark = SparkSession.builder \
     .appName("Movie Consumer") \
     .config("spark.jars.packages", ",".join(packages)) \
     .config("spark.cores.max", "2") \
+    .config("spark.executor.memory", "2g") \
     .getOrCreate()
 
 spark.sparkContext.setLogLevel("Error")
@@ -61,7 +80,7 @@ df_actor = df_actor.withColumn('gender', expr("map_gender_id_to_name(gender)"))
 
 query = df_actor.writeStream \
     .outputMode("append") \
-    .format("console") \
+    .foreachBatch(write_to_elasticsearch) \
     .start()
 
 query.awaitTermination()
--- app/movie_consumer.py
+++ app/movie_consumer.py
@@ -1,10 +1,10 @@
 from pyspark.sql import SparkSession
 from schema import MOVIE_SCHEMA, GENRE_SCHEMA
-from pyspark.sql.functions import explode, col, from_json, current_timestamp, expr, udf
+from pyspark.sql.functions import to_timestamp, from_json, expr, to_date, current_timestamp
 from pyspark.sql.types import StringType, ArrayType
 import os, json
 from dotenv import load_dotenv
-
+import requests
 load_dotenv()
 scala_version = '2.12'
 spark_version = '3.5.0'
@@ -13,8 +13,27 @@ MASTER = 'local'
 # MASTER = 'spark://172.28.240.1:7077'
 KAFKA_BROKER1 = os.environ["KAFKA_BROKER1"]
 MOVIE_TOPIC = os.environ["MOVIE_TOPIC"]
+ES_NODES = os.environ['ES_NODES']
+ES_RESOURCE = "movie"
 genre_path = 'genres.json'
+# ------------------------------------------------------------
 
+def map_genre_ids_to_names(genre_ids):
+    return [genre_names.get(gid, None) for gid in genre_ids]
+
+def write_to_elasticsearch(df, epoch_id):
+    df.show()
+    df.write \
+        .format("org.elasticsearch.spark.sql") \
+        .option("es.nodes", ES_NODES) \
+        .option("es.resource", ES_RESOURCE) \
+        .option("es.index.auto.create", "true") \
+        .option("es.nodes.wan.only", "true") \
+        .mode("overwrite") \
+        .save(ES_RESOURCE)
+
+# ----------------------------------------------------------
+    
 with open(genre_path, 'r') as f:
     genre_names = json.loads(f.read())
 
@@ -24,6 +43,7 @@ packages = [
     f'org.apache.spark:spark-sql-kafka-0-10_{scala_version}:{spark_version}',
     'org.apache.kafka:kafka-clients:3.5.0',
     'org.apache.hadoop:hadoop-client:3.0.0',
+    'org.elasticsearch:elasticsearch-spark-30_2.12:7.17.16'
 ]
 
 spark = SparkSession.builder \
@@ -31,12 +51,11 @@ spark = SparkSession.builder \
     .appName("Movie Consumer") \
     .config("spark.jars.packages", ",".join(packages)) \
     .config("spark.cores.max", "2") \
+    .config("spark.executor.memory", "2g") \
     .getOrCreate()
 
 spark.sparkContext.setLogLevel("Error")
 
-def map_genre_ids_to_names(genre_ids):
-    return [genre_names.get(gid, None) for gid in genre_ids]
 
 # Định nghĩa hàm UDF từ hàm Python
 spark.udf.register("map_genre_ids_to_names", map_genre_ids_to_names, ArrayType(StringType()))
@@ -63,12 +82,13 @@ df_movie = df_movie.withColumn("vote_average", expr("cast(vote_average as double
 
 df_movie = df_movie.withColumn("genres", expr("map_genre_ids_to_names(genre_ids)"))
 
+df_movie = df_movie.withColumn("release_date", to_date("release_date", "yyyy-MM-dd"))
 
 df_movie = df_movie.drop('genre_ids')
 
 query = df_movie.writeStream \
     .outputMode("append") \
-    .format("console") \
+    .foreachBatch(write_to_elasticsearch) \
     .start()
 
 query.awaitTermination()
