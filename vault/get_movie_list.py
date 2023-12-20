96c5f33b604b848ad30eb23c9a234fadd7f9eed2
minhhotboy9x <minhnguyenquocnhat@gmail.com>
2023-12-20 23:32:21 +0700

    10k

--- vault/get_movie_list.py
+++ vault/get_movie_list.py
@@ -3,7 +3,7 @@ sys.path.append('../')
 from app import crawler
 import json
 
-
+# 10000 movies
 def append_to_json(data, file_path):
     cnt = 0
     # print(data)
@@ -17,7 +17,7 @@ if __name__ == "__main__":
     movies = crawler.MovieDB()
     path = 'movies_data.jsonl'
     cnt = 0
-    for i in range(0, 1000):
+    for i in range(0):
         mv_json = movies.get_movies(page=i+1)
         cnt += append_to_json(data=mv_json, file_path=path)
         print(f'{i+1}: got {cnt} movies')
