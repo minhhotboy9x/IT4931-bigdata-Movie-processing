bef23e0a5115dd94833b5979c8298f917f74060d
minhhotboy9x <minhnguyenquocnhat@gmail.com>
2023-12-21 22:33:44 +0700

    'delete_query'

--- vault/delete_query_es.py
+++ vault/delete_query_es.py
@@ -4,21 +4,22 @@ import requests
 elasticsearch_url = "http://localhost:9200"
 
 # Index and endpoint for delete by query
-index_name = "movie"
-delete_by_query_endpoint = f"{elasticsearch_url}/{index_name}/_delete_by_query"
+index_names = ["movie", "actor"]
+for index in index_names:
+    delete_by_query_endpoint = f"{elasticsearch_url}/{index}/_delete_by_query"
 
-# Query to match all documents
-query = {
-    "query": {
-        "match_all": {}
+    # Query to match all documents
+    query = {
+        "query": {
+            "match_all": {}
+        }
     }
-}
 
-# Send the POST request
-response = requests.post(delete_by_query_endpoint, json=query)
+    # Send the POST request
+    response = requests.post(delete_by_query_endpoint, json=query)
 
-# Check the response
-if response.status_code == 200:
-    print("Documents deleted successfully.")
-else:
-    print(f"Error: {response.status_code}, {response.text}")
+    # Check the response
+    if response.status_code == 200:
+        print("Documents deleted successfully.")
+    else:
+        print(f"Error: {response.status_code}, {response.text}")
